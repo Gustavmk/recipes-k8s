5a2c6194b164a6d513d000c11e1a105e4d7240ab
gustavo.kuno <gustavo.mkuno@gmail.com>
2024-11-25 06:24:43 -0300

    wip

--- day-16-taints_tolerations_labels/maintance.sh
+++
@@ -1,4 +0,0 @@
-NODE_NAME='aks-syspool001-57110861-vmss000003'
-kubectl taint nodes $NODE_NAME  maintance=on:NoExecute
-
-kubectl get pods --all-namespaces -o wide --field-selector spec.nodeName=$NODE_NAME
